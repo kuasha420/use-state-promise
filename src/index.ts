01872d1ce4f84419662f795f88a487aefb20297f
Calvin Tennant <calvintennant@gmail.com>
2022-11-10 16:38:19 -0500

    Update index.ts
    Fix useStatePromise such that setStatePromise is guarantee to resolve.

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,41 @@
-import { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
 interface UseStatePromiseFn {
-  <T>(initialState: T | (() => T), skipFirst?: boolean): readonly [
-    T,
-    (value: SetStateAction<T>) => Promise<T>,
-    Dispatch<SetStateAction<T>>
-  ];
-  <T = undefined>(skipFirst?: boolean): readonly [
-    T | undefined,
-    (value: SetStateAction<T | undefined>) => Promise<T | undefined>,
-    Dispatch<SetStateAction<T | undefined>>
-  ];
+    <T>(initialState: T | (() => T)): readonly [
+        T,
+        (value: SetStateAction<T>) => Promise<T>,
+        Dispatch<SetStateAction<T>>
+    ];
+    <T = undefined>(): readonly [
+        T | undefined,
+        (value: SetStateAction<T | undefined>) => Promise<T | undefined>,
+        Dispatch<SetStateAction<T | undefined>>
+    ];
 }
 
-export const useStatePromise: UseStatePromiseFn = <T>(initialState?: T, skipFirst = true) => {
-  const [state, setState] = useState(initialState);
-  const first = useRef(skipFirst);
-  const resolver = useRef<((value: T | undefined) => void) | null>(null);
+export const useStatePromise: UseStatePromiseFn = <T>(initialState?: T) => {
+    const [state, setState] = useState(initialState);
 
-  const setStatePromise = useCallback((value: SetStateAction<T>) => {
-    setState(value);
-    return new Promise<T | undefined>((resolve) => {
-      resolver.current = resolve;
-    });
-  }, []);
+    const setStatePromise = useCallback((value: T & ((prevState: T | undefined) => T)) => {
+        if (typeof value === 'function') {
+            return new Promise((resolve) => {
+                setState((prevState: T | undefined) => {
+                    const nextValue = value(prevState);
+                    resolve(nextValue);
+                    return nextValue;
+                });
+            });
+        } else {
+            return new Promise((resolve) => {
+                setState(() => {
+                    resolve(value);
+                    return value;
+                });
+            });
+        }
+    }, []);
 
-  useEffect(() => {
-    if (first.current) {
-      first.current = false;
-      return;
-    }
-    if (resolver.current) {
-      resolver.current(state);
-      resolver.current = null;
-    }
-  }, [state]);
-
-  return [state, setStatePromise, setState] as const;
+    return [state, setStatePromise, setState] as const;
 };
 
 export default useStatePromise;
